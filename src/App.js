1afcf0c55079ea057c03dabe5e97b61b5c5efdd8
joseph <iossif123gg@gmail.com>
2019-05-05 19:50:10 +0300

    stuff

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React from "react";
+import {state} from "./context";
 
-function App() {
+const Context = React.createContext();
+
+function App(props) {
+  
+  const selectTodo = todo => {
+    console.log(todo);
+  };
+
+  const openModal = todo => {
+    console.log("open modal");
+  };
+
+  const removeTodo = todo => {
+    console.log("remove todo");
+  };
+
   return (
-    <div>
-      j
-    </div>
+    <Context.Provider value={state}>
+      <div className="container">
+        
+        {state.todos.map(todo => {
+          return (
+            <div
+              style={{ backgroundColor: todo.selected ? "lightskyblue" : "" }}
+              className="todo"
+              key={todo.id}
+            >
+              <div>{todo.name}</div>
+              <div>
+                <button onClick={() => selectTodo(todo)}>Select</button>
+                <button onClick={() => openModal(todo)}>Update</button>
+                <button onClick={() => removeTodo(todo)}>Remove</button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </Context.Provider>
   );
 }
 
---
+++ src/context/index.js
@@ -0,0 +1,10 @@
+export const state = {
+  todos: [ // lightskyblue
+    { id: 1, name: "ivan", selected: false, },
+    { id: 2, name: "retard", selected: false, },
+    { id: 3, name: "lina", selected: false, }
+  ],
+  modal: false,
+  auth: false,
+};
+
